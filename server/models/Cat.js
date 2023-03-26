08bb996d35dc2e446963f4c055b5d74ec375cb91
CursedMushroom <osl2877@rit.edu>
2023-03-26 16:35:26 -0400

    created dog modle and functions as well as hndlebar page

--- server/models/Cat.js
+++ server/models/Cat.js
@@ -60,10 +60,6 @@ const CatSchema = new mongoose.Schema({
   },
 
 });
-// Create the cat model based on the schema. You provide it with a custom discriminator
-// (the name of the object type. Can be anything)
-// and the schema to make a model from.
-// Look at the model variable definition above for more details.
 CatModel = mongoose.model('Cat', CatSchema);
 
 // We only want to export the cat model, so we can overwrite the entire exports object.
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,10 @@
 module.exports.Cat = require('./Cat.js');
 
+// const Cat = require('./Cat.js');
+// const Dog = require('./Dog.js');
+
+// module.exports = { Cat, Dog };
+
 // index.js gets automatically pulled when a folder is required
 // We use this to our advantage to define an API for this package.
 // We could use it to pull as many files as necessary and send it back as one API
